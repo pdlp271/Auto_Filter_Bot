5aa7dd5573c6188716c16ae4768e7f0b192bba5e
pdlp271 <109448588+pdlp271@users.noreply.github.com>
2025-06-25 15:39:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -176,9 +176,9 @@ else:
     ON_HEROKU = False
 HAS_SSL = bool(getenv('HAS_SSL', True))
 if HAS_SSL:
-    URL = "https://{}/".format(FQDN)
+    URL = "https://{universal-nonie-hprajbots-1a54aca8.koyeb.app/}/".format(FQDN)
 else:
-    URL = "http://{}/".format(FQDN)
+    URL = "http://{universal-nonie-hprajbots-1a54aca8.koyeb.app/}/".format(FQDN)
 
 # ============================
 # Reactions Configuration
