f22a891f479cfe5e60c6078e51f3d6bf94004dcd
pdlp271 <109448588+pdlp271@users.noreply.github.com>
2026-09-24 18:04:13 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -137,9 +137,9 @@ Nᴀᴍᴇ - {}
 <blockquote>👇 Spelling mistake ಆಗಿದೆ ❗. ಚಿಂತೆ ಮಾಡ್ಬೇಡಿ ಕೆಳಗಿನವುಗಳಲ್ಲಿ ಒಂದನ್ನು ಆಯ್ಕೆ ಮಾಡಿ </blockquote>"""
 
 
-    DEL_MSG = """⚠️ ᴛʜɪꜱ ᴍᴏᴠɪᴇ ꜰɪʟᴇ/ᴠɪᴅᴇᴏ ᴡɪʟʟ ʙᴇ ᴅᴇʟᴇᴛᴇᴅ ɪɴ <b><u><code>{}</code></u></b>
-
-<blockquote expandable><b><i>ᴘʟᴇᴀꜱᴇ ꜰᴏʀᴡᴀʀᴅ ᴛʜɪꜱ ꜰɪʟᴇ ᴛᴏ ꜱᴏᴍᴇᴡʜᴇʀᴇ ᴇʟꜱᴇ & ꜱᴛᴀʀᴛ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴛʜᴇʀᴇ</i></b></blockquote>"""
+    DEL_MSG = """<blockquote expandable><b><i> ⧗ Dᴜᴇ ᴛᴏ Cᴏᴘʏʀɪɢʜᴛ ɪssᴜᴇs....</i></b></blockquote>
+<blockquote expandable><b><i>›› Yᴏᴜʀ ғɪʟᴇs ᴡɪʟʟ ʙᴇ ᴅᴇʟᴇᴛᴇᴅ ᴡɪᴛʜɪɴ <b><u><code>{}</code></u></b>. Sᴏ ғᴏʀᴡᴀʀᴅ ɪᴛ ɪᴍᴍᴇᴅɪᴀᴛᴇʟʏ.</i></b></blockquote>
+<blockquote expandable><b><i>≡ ɴᴏᴛᴇ : ᴜsᴇ 𝐕𝐋𝐂 / 𝐌𝐗 ᴩʟᴀʏᴇʀ ꜰᴏʀ ʙᴇᴛᴛᴇʀ ᴇxᴩᴇʀɪᴇɴᴄᴇ.</i></b></blockquote>"""
 
 
     I_CUDNT = """<b>ɪ ᴄᴀɴ'ᴛ ꜱʀᴇᴀᴄʜ {} 😔
